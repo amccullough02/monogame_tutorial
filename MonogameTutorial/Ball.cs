204b61cfb99cf131a6a044ca63547881caff5ec6
Alex McCullough <alexmccullough@outlook.com>
2024-11-10 20:40:54 +0000

    chore: renamed Ball parameters to meet naming convention

--- MonogameTutorial/Ball.cs
+++ MonogameTutorial/Ball.cs
@@ -7,78 +7,78 @@ namespace MonogameTutorial;
 
 public class Ball
 {
-    Texture2D texture;
-    Vector2 position;
-    private float speed;
+    Texture2D Texture;
+    Vector2 Position;
+    private float Speed;
 
     public Ball(float speed)
     {
-        this.speed = speed;
+        this.Speed = speed;
     }
 
     public void Initialize(GraphicsDeviceManager graphics)
     {
-        position = new Vector2(graphics.PreferredBackBufferWidth / 2, graphics.PreferredBackBufferHeight / 2);
+        Position = new Vector2(graphics.PreferredBackBufferWidth / 2, graphics.PreferredBackBufferHeight / 2);
     }
 
     public void LoadContent(ContentManager content)
     {
-        texture = content.Load<Texture2D>("ball");
+        Texture = content.Load<Texture2D>("ball");
     }
 
     public void Update(GraphicsDeviceManager graphics, GameTime gameTime)
     {
-        float updatedBallSpeed = speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+        float updatedBallSpeed = Speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
         
         var kstate = Keyboard.GetState();
 
         if (kstate.IsKeyDown(Keys.W))
         {
-            position.Y -= updatedBallSpeed;
+            Position.Y -= updatedBallSpeed;
         }
 
         if (kstate.IsKeyDown(Keys.S))
         {
-            position.Y += updatedBallSpeed;
+            Position.Y += updatedBallSpeed;
         }
 
         if (kstate.IsKeyDown(Keys.A))
         {
-            position.X -= updatedBallSpeed;
+            Position.X -= updatedBallSpeed;
         }
 
         if (kstate.IsKeyDown(Keys.D))
         {
-            position.X += updatedBallSpeed;
+            Position.X += updatedBallSpeed;
         }
 
-        if (position.X > graphics.PreferredBackBufferWidth - texture.Width / 2)
+        if (Position.X > graphics.PreferredBackBufferWidth - Texture.Width / 2)
         {
-            position.X = graphics.PreferredBackBufferWidth - texture.Width / 2;
+            Position.X = graphics.PreferredBackBufferWidth - Texture.Width / 2;
         }
-        else if (position.X < texture.Width / 2)
+        else if (Position.X < Texture.Width / 2)
         {
-            position.X = texture.Width / 2;
+            Position.X = Texture.Width / 2;
         }
 
-        if (position.Y > graphics.PreferredBackBufferHeight - texture.Height / 2)
+        if (Position.Y > graphics.PreferredBackBufferHeight - Texture.Height / 2)
         {
-            position.Y = graphics.PreferredBackBufferHeight - texture.Height / 2;
+            Position.Y = graphics.PreferredBackBufferHeight - Texture.Height / 2;
         }
-        else if (position.Y < texture.Height / 2)
+        else if (Position.Y < Texture.Height / 2)
         {
-            position.Y = texture.Height / 2;
+            Position.Y = Texture.Height / 2;
         }
     }
 
     public void Draw(SpriteBatch spriteBatch)
     {
-        spriteBatch.Draw(texture, 
-            position,
+        spriteBatch.Draw(Texture, 
+            Position,
             null,
             Color.White,
             0f,
-            new Vector2(texture.Width / 2, texture.Height / 2),
+            new Vector2(Texture.Width / 2, Texture.Height / 2),
             Vector2.One,
             SpriteEffects.None,
             0f);
